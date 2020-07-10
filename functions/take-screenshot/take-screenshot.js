74373d74b1430be79b80b68b2ee86942e285444a
Kostadin Terziev <kostadin_terziev@abv.bg>
2020-07-10 12:34:58 +0300

    .

--- functions/take-screenshot/take-screenshot.js
+++ functions/take-screenshot/take-screenshot.js
@@ -3,35 +3,12 @@ import puppeteer from "puppeteer-serverless";
 import renderSocialImage from "puppeteer-social-image";
 
 exports.handler = async (event, context) => {
+    const pageToScreenshot = JSON.parse(event.body).pageToScreenshot;
 
-    // const pageToScreenshot = JSON.parse(event.body).pageToScreenshot;
-
-    // if (!pageToScreenshot) return {
-    //     statusCode: 400,
-    //     body: JSON.stringify({ message: 'Page URL not defined' })
-    // }
-
-    // const browser = await chromium.puppeteer.launch({
-    //     args: chromium.args,
-    //     defaultViewport: chromium.defaultViewport,
-    //     executablePath: await chromium.executablePath,
-    //     headless: chromium.headless,
-    // });
-
-    // const page = await browser.newPage();
-
-    // await page.goto(pageToScreenshot, { waitUntil: 'networkidle2' });
-
-    // const screenshot =  await page.screenshot({
-    //     clip: {
-    //       x: 0,
-    //       y: 0,
-    //       width: 1200,
-    //       height: 600,
-    //     },
-    //   })
-
-    // await browser.close();
+    if (!pageToScreenshot) return {
+        statusCode: 400,
+        body: JSON.stringify({ message: 'Page URL not defined' })
+    }
 
     const browser = await renderSocialImage({
         template: "basic",
