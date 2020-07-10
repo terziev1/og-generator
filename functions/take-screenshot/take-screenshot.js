67d77f5556dedbc799d001a049c096c52890a499
Kostadin Terziev <kostadin_terziev@abv.bg>
2020-07-10 13:08:20 +0300

    .

--- functions/take-screenshot/take-screenshot.js
+++ functions/take-screenshot/take-screenshot.js
@@ -8,28 +8,26 @@ exports.handler = async (event, context) => {
         statusCode: 400,
         body: JSON.stringify({ message: 'Page URL not defined' })
     }
+    const browser = await chromium.puppeteer.launch({
+                args: chromium.args,
+                defaultViewport: chromium.defaultViewport,
+                executablePath: await chromium.executablePath,
+                headless: chromium.headless,
+            });
 
-    const page = await renderSocialImage({
+    const screenshot = await renderSocialImage({
         templateBody: '<div class="Main">Hello, {{name}}!</div>',
         templateStyles: ".Main { color: blue; }",
         templateParams: {
           name: "Jane"
         },
-        browser: await chromium.puppeteer.launch({
-
-                    args: chromium.args,
-                    defaultViewport: chromium.defaultViewport,
-                    executablePath: await chromium.executablePath,
-                    headless: chromium.headless,
-
-        })
+        browser:browser
       });
 
     // const page = await browser.newPage();
 
-    await page.goto(pageToScreenshot, { waitUntil: 'networkidle2' });
 
-    const screenshot =  await page.screenshot({})
+
 
     await browser.close();
 
