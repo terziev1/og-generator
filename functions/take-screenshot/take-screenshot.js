fe28af038d3a77a8b632cbc15d4c38ad2279acec
Kostadin Terziev <kostadin_terziev@abv.bg>
2020-07-10 13:17:28 +0300

    .

--- functions/take-screenshot/take-screenshot.js
+++ functions/take-screenshot/take-screenshot.js
@@ -1,36 +1,35 @@
 const chromium = require('chrome-aws-lambda');
-const renderSocialImage = require('puppeteer-social-image');
 
 exports.handler = async (event, context) => {
+
     const pageToScreenshot = JSON.parse(event.body).pageToScreenshot;
 
     if (!pageToScreenshot) return {
         statusCode: 400,
         body: JSON.stringify({ message: 'Page URL not defined' })
     }
+
     const browser = await chromium.puppeteer.launch({
-                args: chromium.args,
-                defaultViewport: chromium.defaultViewport,
-                executablePath: await chromium.executablePath,
-                headless: chromium.headless,
-            });
+        args: chromium.args,
+        defaultViewport: chromium.defaultViewport,
+        executablePath: await chromium.executablePath,
+        headless: chromium.headless,
+    });
 
-    const screenshot = await renderSocialImage({
-        templateBody: '<div class="Main">Hello, {{name}}!</div>',
-        templateStyles: ".Main { color: blue; }",
-        templateParams: {
-          name: "Jane"
+    const page = await browser.newPage();
+
+    await page.goto(pageToScreenshot, { waitUntil: 'networkidle2' });
+
+    const screenshot =   await page.screenshot({
+        clip: {
+          x: 0,
+          y: 0,
+          width: 1200,
+          height: 600,
         },
-        browser:browser
-      });
-
-    // const page = await browser.newPage();
-
-
-
+      })
 
     await browser.close();
-
 
     return {
         statusCode: 200,
@@ -40,6 +39,4 @@ exports.handler = async (event, context) => {
         })
     }
 
-
 }
-
