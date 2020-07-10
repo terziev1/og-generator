3a6f8b53fc66061e060397453f85c45ddb0c9631
Kostadin Terziev <kostadin_terziev@abv.bg>
2020-07-10 12:22:54 +0300

    .

--- functions/take-screenshot/take-screenshot.js
+++ functions/take-screenshot/take-screenshot.js
@@ -1,42 +1,47 @@
 const chromium = require('chrome-aws-lambda');
+import puppeteer from "puppeteer-serverless";
+import renderSocialImage from "puppeteer-social-image";
 
 exports.handler = async (event, context) => {
 
-    const pageToScreenshot = JSON.parse(event.body).pageToScreenshot;
+    // const pageToScreenshot = JSON.parse(event.body).pageToScreenshot;
 
-    if (!pageToScreenshot) return {
-        statusCode: 400,
-        body: JSON.stringify({ message: 'Page URL not defined' })
-    }
+    // if (!pageToScreenshot) return {
+    //     statusCode: 400,
+    //     body: JSON.stringify({ message: 'Page URL not defined' })
+    // }
 
-    const browser = await chromium.puppeteer.launch({
-        args: chromium.args,
-        defaultViewport: chromium.defaultViewport,
-        executablePath: await chromium.executablePath,
-        headless: chromium.headless,
-    });
+    // const browser = await chromium.puppeteer.launch({
+    //     args: chromium.args,
+    //     defaultViewport: chromium.defaultViewport,
+    //     executablePath: await chromium.executablePath,
+    //     headless: chromium.headless,
+    // });
 
-    const page = await browser.newPage();
+    // const page = await browser.newPage();
 
-    await page.goto(pageToScreenshot, { waitUntil: 'networkidle2' });
+    // await page.goto(pageToScreenshot, { waitUntil: 'networkidle2' });
 
-    const screenshot =  await page.screenshot({
-        clip: {
-          x: 0,
-          y: 0,
-          width: 1200,
-          height: 600,
+    // const screenshot =  await page.screenshot({
+    //     clip: {
+    //       x: 0,
+    //       y: 0,
+    //       width: 1200,
+    //       height: 600,
+    //     },
+    //   })
+
+    // await browser.close();
+   return await renderSocialImage({
+        template: "basic",
+        templateParams: {
+          imageUrl:
+            "https://images.unsplash.com/photo-1557958114-3d2440207108?w=1950&q=80",
+          title: "Hello, world"
         },
-      })
+        browser: await puppeteer.launch({})
+      });
 
-    await browser.close();
-
-    return {
-        statusCode: 200,
-        body: JSON.stringify({
-            message: `Complete screenshot of ${pageToScreenshot}`,
-            buffer: screenshot
-        })
-    }
 
 }
+
--- public/script.js
+++ public/script.js
@@ -1,10 +1,10 @@
 function bufferToImageUrl(buffer) {
     // See https://gist.github.com/candycode/f18ae1767b2b0aba568e
 
-    var arrayBufferView = new Uint8Array( buffer );
-    var blob = new Blob( [ arrayBufferView ], { type: "image/jpeg" } );
+    var arrayBufferView = new Uint8Array(buffer);
+    var blob = new Blob([arrayBufferView], { type: "image/jpeg" });
     var urlCreator = window.URL || window.webkitURL;
-    var imageUrl = urlCreator.createObjectURL( blob );
+    var imageUrl = urlCreator.createObjectURL(blob);
 
     return imageUrl;
 }
@@ -26,16 +26,16 @@ document.querySelector('button[type="submit"]').addEventListener('click', (e) =>
     document.getElementById('result').textContent = "Please wait..."
 
     fetch("/.netlify/functions/take-screenshot", options)
-        .then((res) => res.json())
-        .then((res) => {
+        .then((res) => console.log(res))
+        // .then((res) => {
 
-if (!res.buffer) return document.getElementById('result').textContent = 'Error capturing screenshot';
+        //     if (!res.buffer) return document.getElementById('result').textContent = 'Error capturing screenshot';
 
-const img = document.createElement('img');
-img.src = bufferToImageUrl(res.buffer.data);
-document.getElementById('result').innerHTML = img.outerHTML;
-})
-.catch((err) => {
+        //     const img = document.createElement('img');
+        //     img.src = bufferToImageUrl(res.buffer.data);
+        //     document.getElementById('result').innerHTML = img.outerHTML;
+        // })
+        .catch((err) => {
             console.log(err)
             document.getElementById('result').textContent = `Error: ${err.toString()}`
         });
