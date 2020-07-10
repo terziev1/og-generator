474ffcce7343a10f8a149778dcda843ff66cddc9
Kostadin Terziev <kostadin_terziev@abv.bg>
2020-07-10 12:31:41 +0300

    .

--- functions/take-screenshot/take-screenshot.js
+++ functions/take-screenshot/take-screenshot.js
@@ -32,15 +32,38 @@ exports.handler = async (event, context) => {
     //   })
 
     // await browser.close();
-   return await renderSocialImage({
+
+    const browser = await renderSocialImage({
         template: "basic",
         templateParams: {
           imageUrl:
             "https://images.unsplash.com/photo-1557958114-3d2440207108?w=1950&q=80",
           title: "Hello, world"
         },
-        browser: await puppeteer.launch({})
+        browser: await puppeteer.launch({
+
+                    args: chromium.args,
+                    defaultViewport: chromium.defaultViewport,
+                    executablePath: await chromium.executablePath,
+                    headless: chromium.headless,
+
+        })
       });
+          const page = await browser.newPage();
+
+    await page.goto(pageToScreenshot, { waitUntil: 'networkidle2' });
+
+    const screenshot =  await page.screenshot({})
+
+    await browser.close();
+
+    return {
+        statusCode: 200,
+        body: JSON.stringify({
+            message: `Complete screenshot of ${pageToScreenshot}`,
+            buffer: screenshot
+        })
+    }
 
 
 }
